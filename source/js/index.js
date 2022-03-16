fb33fe78ad7d400f32d34d09438261058c529b87
carlos260800 <mash.x2003@gmail.com>
2022-03-15 22:06:40 -0600

    primera version

--- source/js/index.js
+++ source/js/index.js
@@ -4,18 +4,46 @@ const fetchPokemon = () => {
     const url = `https://pokeapi.co/api/v2/pokemon/${pokeInput}`;
     fetch(url).then((res) => {
         //console.log(res);
-        if(res.status!="200"){
+        if (res.status != "200") {
             console.log(res);
-            pokeImage("../source/img/logo.png");
+            pokeImage("../source/img/unu.jpeg");
         }
-        else{
+        else {
             return res.json();
         }
     }).then((data) => {
         console.log(data);
         let pokeImg = data.sprites.front_default;
-        console.log(pokeImg);
+        //console.log(pokeImg);
+        let nombre = data.name;
+        let id=data.id;
+        let tipo = data.types[0].type.name;
+        let hp = data.stats[0].base_stat;
+        let ataque = data.stats[1].base_stat;
+        let defensa = data.stats[2].base_stat;
+        let ataqueEspecial= data.stats[3].base_stat;
+        let defensaEspecial = data.stats[4].base_stat;
+        let habilidad1=data.abilities[0].ability.name;
+        let habilidad2=data.abilities[1].ability.name;
         pokeImage(pokeImg);
+        document.getElementById("nombre").innerHTML=nombre;
+        document.getElementById("id").innerHTML=id;
+        document.getElementById("pokeTipo").innerHTML=tipo;
+        document.getElementById("pokeHp").innerHTML=hp;
+        document.getElementById("pokeAtaque").innerHTML=ataque;
+        document.getElementById("pokeDefensa").innerHTML=defensa;
+        document.getElementById("pokeAtaqueEspecial").innerHTML=ataqueEspecial;
+        document.getElementById("pokeDefensaEspecial").innerHTML=defensaEspecial;
+        document.getElementById("habilidad1").innerHTML=habilidad1;
+        document.getElementById("habilidad2").innerHTML=habilidad2;
+        //pokeTipo(tipo);
+        // console.log(nombre);
+        // console.log(tipo);
+        // console.log(hp);
+        // console.log(ataque);
+        // console.log(defensa);
+        //console.log(habilidades);
+        //console.log(id);
     })
 }
 
